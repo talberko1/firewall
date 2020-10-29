9a9197c8f9ed04af0b7bf1dac962c3602940f2b5
Tal Berkovitch <tal@localhost.localdomain>
2020-10-29 23:54:36 +0200

    added the server targeted thread class

---
+++ client/include/TargetedThread.h
@@ -0,0 +1,32 @@
+//
+// Created by tal on 10/28/20.
+//
+
+#ifndef CLIENT_TARGETEDTHREAD_H
+#define CLIENT_TARGETEDTHREAD_H
+
+#include "DpdkDeviceList.h"
+#include "PcapFileManager.h"
+#include "IDpdkEndDevice.h"
+#include "EthLayer.h"
+
+class TargetedThread : public pcpp::DpdkWorkerThread {
+private:
+    IDpdkEndDevice *m_Device;
+    bool m_Stop;
+    uint32_t m_CoreId;
+
+    const char *m_Output;
+public:
+    TargetedThread(IDpdkEndDevice *device, const char *output);
+
+    ~TargetedThread() {}
+
+    bool run(uint32_t coreId) override;
+
+    void stop() override;
+
+    uint32_t getCoreId() const override;
+};
+
+#endif //CLIENT_TARGETEDTHREAD_H
