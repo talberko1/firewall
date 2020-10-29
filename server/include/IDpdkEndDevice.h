6cd3ae1495d2ce05eab5afc599677e3a899315ac
Tal Berkovitch <tal@localhost.localdomain>
2020-10-30 00:03:44 +0200

    Same for the client side, I changed how the interface methods should be

--- server/include/IDpdkEndDevice.h
+++ server/include/IDpdkEndDevice.h
@@ -6,17 +6,18 @@
 #define SERVER_IENDDEVICE_H
 
 #include "DpdkDevice.h"
+#include <cstdlib>
 
 class IDpdkEndDevice {
 public:
+
     virtual uint16_t receive(pcpp::MBufRawPacket **packets, uint16_t length, uint16_t queueId) = 0;
 
-    virtual void process(pcpp::Packet packet) = 0;
+    virtual void process(pcpp::MBufRawPacket **packets, uint16_t received) = 0;
 
     virtual uint16_t send(pcpp::MBufRawPacket **packets, uint16_t length, uint16_t queueId) = 0;
 
     ~IDpdkEndDevice() = default;
-
 };
 
 #endif //SERVER_IENDDEVICE_H
