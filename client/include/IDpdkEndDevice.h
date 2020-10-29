a7187a3f07fc21424a6fd5b0e1534ba2731672b9
Tal Berkovitch <tal@localhost.localdomain>
2020-10-30 00:00:55 +0200

    changed the interface methods a bit to be comfortable to use inside worker threads

--- client/include/IDpdkEndDevice.h
+++ client/include/IDpdkEndDevice.h
@@ -4,18 +4,16 @@
 
 #ifndef CLIENT_IENDDEVICE_H
 #define CLIENT_IENDDEVICE_H
+
 #include <cstdlib>
 #include "DpdkDevice.h"
 
 class IDpdkEndDevice {
 public:
-    virtual pcpp::MBufRawPacket *decapsulatePacket(pcpp::Packet &packet) = 0;
 
     virtual uint16_t receive(pcpp::MBufRawPacket **packets, uint16_t length, uint16_t queueId) = 0;
 
-    virtual bool process(pcpp::MBufRawPacket* packet) = 0;
-
-    virtual pcpp::MBufRawPacket *encapsulatePacket(pcpp::MBufRawPacket *packet) = 0;
+    virtual void process(pcpp::MBufRawPacket **packets, uint16_t received) = 0;
 
     virtual uint16_t send(pcpp::MBufRawPacket **packets, uint16_t length, uint16_t queueId) = 0;
 
