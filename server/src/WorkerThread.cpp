e0f21dc82896cfce64aff310c477fb1b7caca1a2
Tal Berkovitch <tal@localhost.localdomain>
2020-10-30 00:04:38 +0200

    minor update

--- server/src/WorkerThread.cpp
+++ server/src/WorkerThread.cpp
@@ -1,23 +1,31 @@
 
 
 #include "WorkerThread.h"
+#include <iostream>
 
-WorkerThread::WorkerThread(IDpdkEndDevice* device) :
+WorkerThread::WorkerThread(IDpdkEndDevice *device) :
         m_Device(device), m_Stop(true), m_CoreId(MAX_NUM_OF_CORES - 1) {}
 
 
 bool WorkerThread::run(uint32_t coreId) {
-    m_CoreId = coreId;
-    m_Stop = false;
-    pcpp::MBufRawPacket *packets[MAX_RECEIVE_BURST];
-    while (!m_Stop) {
-        uint16_t received = m_Device->receive(packets, MAX_RECEIVE_BURST, 0);
-        for (int i = 0; i < received; i++) {
-            // Actual packet that was captured on client side
-            auto payload = (pcpp::MBufRawPacket *) packets[i]->getRawData();
-            m_Device->process(payload);
+    try {
+        m_CoreId = coreId;
+        m_Stop = false;
+        pcpp::MBufRawPacket *packets[64] = {};
+        while (!m_Stop) {
+            uint16_t received = m_Device->receive(packets, 64, 0);
+            std::cout << "received: " << received << std::endl;
+            if (received) {
+                std::cout << received << std::endl;
+                m_Device->process(packets, received);
+                m_Device->send(packets, received, 0);
+            }
         }
-        m_Device->send(packets, received, 0);
+    }
+    catch (std::exception &e) {
+        std::cerr << "ERROR in file: " << __FILE__ << std::endl;
+        std::cerr << "in line: " << __LINE__ << std::endl;
+        std::cerr << "Exception caught: " << e.what() << std::endl;
     }
     return true;
 }
