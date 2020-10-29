162235173a7af9f8a2d447089edb0d249555fe2d
Tal Berkovitch <tal@localhost.localdomain>
2020-10-30 00:02:25 +0200

    This thread was renamed from WorkerThread into GeneralTrafficThread as it was splitted into this and TargetedThread

--- client/src/GeneralTrafficThread.cpp
+++ client/src/GeneralTrafficThread.cpp
@@ -14,13 +14,15 @@ bool GeneralTrafficThread::run(uint32_t coreId) {
     pcpp::PcapFileWriterDevice writer(m_Output);
     PcapFileManager::openPcapWriter(writer);
 
-    pcpp::MBufRawPacket *packets[64];
+    pcpp::MBufRawPacket *packets[64] = {};
     while (!m_Stop) {
         uint16_t received = m_Device->receive(packets, 64, 0);
-        for (int i = 0; i < received; i++) {
-            writer.writePacket(*packets[i]);
+        if (received) {
+            for (int i = 0; i < received; i++) {
+                writer.writePacket(*packets[i]);
+            }
+            m_Device->send(packets, received, 0);
         }
-        m_Device->send(packets, received, 0);
     }
     writer.close();
     return true;
