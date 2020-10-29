ebfb6cce1db0f770424a7ae9f59987ab8cee9638
Tal Berkovitch <tal@localhost.localdomain>
2020-10-30 00:01:33 +0200

    added the updated source

--- client/src/DpdkClient.cpp
+++ client/src/DpdkClient.cpp
@@ -4,59 +4,47 @@
 
 #include "DpdkClient.h"
 
-DpdkClient::DpdkClient(int rxDeviceId, int txDeviceId, const char *serverIp, uint16_t port) : m_RxDevice(
-        DpdkDeviceManager::getDevice(rxDeviceId)),
-                                                                                              m_TxDevice(
-                                                                                                      DpdkDeviceManager::getDevice(
-                                                                                                              txDeviceId)),
-                                                                                              m_ServerAddress(serverIp),
-                                                                                              m_Port(port) {
-    DpdkDeviceManager::openMultiQueues(m_RxDevice, 1, 1);
-    if (m_RxDevice != m_TxDevice) {
-        DpdkDeviceManager::openMultiQueues(m_TxDevice, 1, 1);
-    }
+DpdkClient::DpdkClient(int dpdkDeviceId, const char *serverMacAddress) :
+        m_DpdkDevice(DpdkDeviceManager::getDevice(dpdkDeviceId)), m_ServerMacAddress(serverMacAddress) {
+    DpdkDeviceManager::openMultiQueues(m_DpdkDevice, 1, 1);
+    std::cout << "Client MAC address: " << m_DpdkDevice->getMacAddress().toString() << std::endl;
 }
 
 uint16_t DpdkClient::receive(pcpp::MBufRawPacket **packets, uint16_t length, uint16_t queueId) {
-    return m_RxDevice->receivePackets(packets, length, queueId);
+    return m_DpdkDevice->receivePackets(packets, length, queueId);
 }
 
-pcpp::MBufRawPacket *DpdkClient::decapsulatePacket(pcpp::Packet &packet) {
-    pcpp::PayloadLayer *payload = packet.getLayerOfType<pcpp::PayloadLayer>();
-    return (pcpp::MBufRawPacket *) (payload->getPayload());
-}
-
-bool DpdkClient::process(pcpp::MBufRawPacket* packet) {
-    pcpp::Packet parsed(packet);
-    pcpp::IPv4Layer *ipLayer = parsed.getLayerOfType<pcpp::IPv4Layer>();
-    if (ipLayer->getSrcIpAddress() == m_ServerAddress && ipLayer->getProtocol() == pcpp::TCP) {
-        pcpp::MBufRawPacket* original = decapsulatePacket(parsed);
-        save(original);
+void DpdkClient::process(pcpp::MBufRawPacket **packets, uint16_t received) {
+    int i = 0;
+    bool receivedResponse = false;
+    for (; i < received && !receivedResponse; i++) {
+        pcpp::Packet parsedPacket(packets[i]);
+        pcpp::EthLayer *ethernetLayer = parsedPacket.getLayerOfType<pcpp::EthLayer>();
+        receivedResponse = ethernetLayer->getSourceMac() == m_ServerMacAddress && ethernetLayer->getDestMac() == m_DpdkDevice->getMacAddress();
     }
-    else {
-        pcpp::MBufRawPacket* encapsulated = encapsulatePacket(packet);
-        std::cout << encapsulated << std::endl;
+    if (receivedResponse) {
+        pcpp::Packet parsedPacket(packets[i - 1]);
+        pcpp::PayloadLayer *payloadLayer = parsedPacket.getLayerOfType<pcpp::PayloadLayer>();
+        uint8_t *data = payloadLayer->getPayload();
+        size_t length = payloadLayer->getPayloadLen();
+        PcapFileCreator::createFileFromBytes("filtered.pcap", data, length);
+        stopCapture();
     }
-    return true;
-}
-
-pcpp::MBufRawPacket* DpdkClient::encapsulatePacket(pcpp::MBufRawPacket* packet) {
-    pcpp::Packet encapsulated;
-    pcpp::EthLayer* newEthernetLayer = new pcpp::EthLayer(pcpp::MacAddress(m_RxDevice->getMacAddress()), pcpp::MacAddress(m_TxDevice->getMacAddress()));
-    pcpp::TcpLayer* newTcpLayer = new pcpp::TcpLayer(m_Port + 1, m_Port);
-    encapsulated.addLayer(newEthernetLayer);
-    // add ip layer between tcp & ethernet
-    encapsulated.addLayer(newTcpLayer);
-    return (pcpp::MBufRawPacket*) encapsulated.getRawPacket();
 }
 
 uint16_t DpdkClient::send(pcpp::MBufRawPacket **packets, uint16_t length, uint16_t queueId) {
-    return m_TxDevice->sendPackets(packets, length, queueId);
+    return m_DpdkDevice->sendPackets(packets, length, queueId);
 }
 
-void DpdkClient::startCapture(const char *output) {
+void DpdkClient::startGeneralThread(const char *output) {
     std::vector<pcpp::DpdkWorkerThread *> workers;
-    workers.push_back(new WorkerThread(this, output));
+    workers.push_back(new GeneralTrafficThread(this, output));
+    DpdkDeviceManager::startDpdkWorkerThreads(workers);
+}
+
+void DpdkClient::startTargetedThread(const char *output) {
+    std::vector<pcpp::DpdkWorkerThread *> workers;
+    workers.push_back(new TargetedThread(this, output));
     DpdkDeviceManager::startDpdkWorkerThreads(workers);
 }
 
@@ -64,24 +52,6 @@ void DpdkClient::stopCapture() {
     DpdkDeviceManager::stopDpdkWorkerThreads();
 }
 
-void DpdkClient::save(pcpp::MBufRawPacket *packet) {
+void DpdkClient::sendPcapFile(const char *input) {
 
-}
-
-
-void DpdkClient::send(const char *input) {
-    pcpp::IFileReaderDevice *reader(pcpp::IFileReaderDevice::getReader(input));
-    PcapFileManager::openPcapReader(reader);
-    pcpp::RawPacket pdu;
-    pcpp::MBufRawPacket packet;
-    while (reader->getNextPacket(pdu)) {
-        packet.setRawData((const uint8_t *) &pdu, sizeof(pdu), pdu.getPacketTimeStamp());
-        pcpp::Packet parsed(&packet);
-        pcpp::IPv4Layer *ipLayer = parsed.getLayerOfType<pcpp::IPv4Layer>();
-        ipLayer->setDstIpAddress(m_ServerAddress);
-        pcpp::TcpLayer *tcpLayer = parsed.getLayerOfType<pcpp::TcpLayer>();
-        tcpLayer->getTcpHeader()->portSrc = m_Port;
-    }
-    reader->close();
-    delete reader;
 }
